eabe0de9d747745bc0685d34cae9993d77c8839b
CrisRill <criseriill@gmail.com>
2026-03-25 20:05:32 +0800

    add testing page

--- app/(public)/clinics/testing/page.js
+++ app/(public)/clinics/testing/page.js
@@ -1,13 +1,20 @@
 // import {ClinicCard} from '@/components/ui/ClinicCard';
 import Card from '@/components/ui/GeneralCard';
+import CardTest from '@/components/testing/CardTest';
 
 export default function Testing(){
   return(
     <>
     {/* <ClinicCard />; */}
-    <Card className="m-10 w-[50vw]" style={{border:"1px solid black"}}>
+    {/* <Card className="m-10 w-[50vw]" style={{border:"1px solid black"}}>
       <p>Testing</p>
-    </Card>;
+    </Card>; */}
+    <div className="grid grid-cols-3 gap-4 mx-10">
+    <CardTest title="Jade Kyll" description="BSCS Student 3rd year" className="my-10"></CardTest>
+    <CardTest title="Joseph Christian" description="BSCS Student 3rd year" className="my-10"></CardTest>
+    <CardTest title="Nova Grace" description="BSCS Student 3rd year" className="my-10"></CardTest>
+    </div>
+
     </>
    )
 }
