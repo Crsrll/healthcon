6607b4026506391e9200bd83d68c6fe26ba6cbf0
CrisRill <criseriill@gmail.com>
2026-03-29 23:41:47 +0800

    streak purposes

---
+++ components/testing/float.js
@@ -0,0 +1,9 @@
+export default function floatPage(){
+  return(
+    <>
+    <p>
+    I float above the sky. This is only to light up my github green so sue me mader fader. I am a float.
+    </p>
+    </>
+  )
+}
